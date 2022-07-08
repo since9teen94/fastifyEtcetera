7b9cba6fcd370dbe6b3cae50478b1c97c8610c3f
since9teen94 <droyanez@gmail.com>
2022-07-08 15:09:34 -0500

    server deploy

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 3000;
 const server = require("./app")({
-  logger: { level: "info", transport: { target: "pino-pretty" } },
+  //logger: { level: "info", transport: { target: "pino-pretty" } },
+  logger: true,
 });
 
 server.ready(() => {
