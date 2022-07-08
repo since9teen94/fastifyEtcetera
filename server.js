eb5fa8a1881936e323488396d3eff7580b93cc4d
since9teen94 <droyanez@gmail.com>
2022-07-08 15:15:55 -0500

    more

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 "use strict";
-const dotenv = require("dotenv").config();
+//const dotenv = require("dotenv").config();
 const port = process.env.PORT || 3000;
 const server = require("./app")({
   //logger: { level: "info", transport: { target: "pino-pretty" } },
