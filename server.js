4f2d1a946ba457b1989c79669ab957a231686f3c
since9teen94 <droyanez@gmail.com>
2022-07-08 15:36:51 -0500

    more

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 "use strict";
 const dotenv = require("dotenv").config();
-//const port = process.env.PORT || 3000;
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || "0.0.0.0";
 const server = require("./app")({
   //logger: { level: "info", transport: { target: "pino-pretty" } },
   logger: true,
 });
 
 server.ready(() => {
-  //server.listen({ port }, () => console.log());
-  server.listen(process.env.PORT, "0.0.0.0");
+  server.listen({ port, host }, () =>
+    console.log(`Listening on port: ${port}`)
+  );
 });
