92f09300d3b4143e83a267b2954624da3d3920ac
since9teen94 <droyanez@gmail.com>
2022-07-08 15:41:06 -0500

    Deploy settings set successfully, heroku

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const dotenv = require("dotenv").config();
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || "0.0.0.0";
 const server = require("./app")({
-  //logger: { level: "info", transport: { target: "pino-pretty" } },
   logger: true,
 });
 
