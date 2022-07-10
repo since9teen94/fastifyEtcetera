9ad90ef48a7dcd8e57eea90861e44ca6b0aa3514
since9teen94 <droyanez@gmail.com>
2022-07-09 19:31:29 -0500

    Heroku postgres db addition

--- plugins/postgres.js
+++ plugins/postgres.js
@@ -1,6 +1,8 @@
 const fp = require("fastify-plugin");
 module.exports = fp(async (app, opts) => {
+  //for deploy
+  const connectionString = app.env("DATABASE_URL") || app.env("CONN_STRING");
   app.register(require("@fastify/postgres"), {
-    connectionString: app.env("CONN_STRING"),
+    connectionString,
   });
 });
