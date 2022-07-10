b9966713294c849a683f78631d17c818f14de16e
since9teen94 <droyanez@gmail.com>
2022-07-09 23:09:39 -0500

    more

--- plugins/postgres.js
+++ plugins/postgres.js
@@ -2,8 +2,8 @@ const fp = require("fastify-plugin");
 module.exports = fp(async (app, opts) => {
   //for deploy
   const connectionString = app.env("DATABASE_URL") || app.env("CONN_STRING");
-  let opts = { connectionString };
-  if (connectionString == app.env("DATABASE_URL"))
-    opts.rejectUnauthorized = true;
-  app.register(require("@fastify/postgres"), {...opts});
+  app.register(require("@fastify/postgres"), {
+    connectionString,
+    ssl: { rejectUnauthorized: true },
+  });
 });
